a9be474304798f80179aa58babd3a172c4ff51b8
Juan Ml. Morillo <juanmorillios@gmail.com>
2014-11-16 16:52:15 +0100

    Rev 1.4
    Rev 1.4

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mailButton: UIButton!
    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -40,12 +43,23 @@ class ViewController: UIViewController {
     // asignamos el contenido del TextField MEnsaje y al mismo tiempo lo presentamos con otro color.
     messageLabel.text = enterMessageTextField.text
     messageLabel.textColor = UIColor .redColor()
+
+    nameLabel.hidden = false
+    nameLabel.text = enterNameTextField.text
+    nameLabel.textColor = UIColor.blueColor()
         
+    //Ocultar teclado una vez se escriba el nombre
+        
+    nameLabel.resignFirstResponder()
+    
+
+    //Ocultar teclado una vez se escriba el mensaje
     enterMessageTextField.resignFirstResponder()
 
     mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
-        
+    
+    
     
         
     }
